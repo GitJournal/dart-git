c174eac4f528035e4f5b64ad9a59e55b1d546e02
Vishesh Handa <me@vhanda.in>
2020-02-28 17:34:53 +0100

    Move command code to main file

--- lib/git.dart
+++ lib/git.dart
@@ -1,67 +1,9 @@
 import 'dart:io';
 import 'dart:convert';
 
-import 'package:args/args.dart';
-
 import 'package:path/path.dart' as p;
 import 'package:ini/ini.dart';
 import 'package:crypto/crypto.dart';
-
-void main(List<String> args) async {
-  var parser = ArgParser();
-  var catFileCommand = ArgParser();
-
-  var hashObjectCommand = ArgParser();
-  hashObjectCommand.addOption(
-    'type',
-    abbr: 't',
-    defaultsTo: 'blob',
-    allowed: ['blob', 'commit', 'tag', 'tree'],
-    help: 'Specify the type',
-  );
-  hashObjectCommand.addFlag(
-    'write',
-    abbr: 'w',
-    defaultsTo: true,
-    help: 'Actually writes the object into the database',
-  );
-
-  parser.addCommand('cat-file', catFileCommand);
-  parser.addCommand('hash-object', hashObjectCommand);
-
-  var results = parser.parse(args);
-  var cmd = results.command;
-  if (cmd == null) {
-    print(parser.usage);
-    exit(0);
-  }
-
-  if (cmd.name == 'cat-file') {
-    var repo = GitRepository(Directory.current.path);
-
-    var objectSha1 = cmd.arguments[1];
-    var obj = await repo.readObjectFromSha(objectSha1);
-    if (obj is GitBlob) {
-      var s = utf8.decode(obj.blobData);
-      print(s);
-    }
-  }
-
-  if (cmd.name == 'hash-object') {
-    if (cmd.rest.isEmpty) {
-      print('Must provide file path');
-      return;
-    }
-    var filePath = cmd.rest[0];
-    var rawData = File(filePath).readAsBytesSync();
-
-    var repo = GitRepository(Directory.current.path);
-    var fmt = cmd['type'] as String;
-    var obj = repo.createObject(fmt, rawData);
-    var sha1Hash = await repo.writeObject(obj, write: cmd['write']);
-    print(sha1Hash);
-  }
-}
 
 class GitRepository {
   String workTree;
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,61 @@
 import 'dart:io';
+import 'dart:convert';
 import 'git.dart';
 
+import 'package:args/args.dart';
+
 void main(List<String> args) async {
-  print(args);
-  var cmd = args[0];
-  if (cmd == 'init') {
-    var path = args[1];
-    await GitRepository.init(path);
-    print('Done');
+  var parser = ArgParser();
+  var catFileCommand = ArgParser();
+
+  var hashObjectCommand = ArgParser();
+  hashObjectCommand.addOption(
+    'type',
+    abbr: 't',
+    defaultsTo: 'blob',
+    allowed: ['blob', 'commit', 'tag', 'tree'],
+    help: 'Specify the type',
+  );
+  hashObjectCommand.addFlag(
+    'write',
+    abbr: 'w',
+    defaultsTo: true,
+    help: 'Actually writes the object into the database',
+  );
+
+  parser.addCommand('cat-file', catFileCommand);
+  parser.addCommand('hash-object', hashObjectCommand);
+
+  var results = parser.parse(args);
+  var cmd = results.command;
+  if (cmd == null) {
+    print(parser.usage);
+    exit(0);
   }
 
-  if (cmd == 'cat-file') {
-    var sha1 = args[1];
+  if (cmd.name == 'cat-file') {
+    var repo = GitRepository(Directory.current.path);
+
+    var objectSha1 = cmd.arguments[1];
+    var obj = await repo.readObjectFromSha(objectSha1);
+    if (obj is GitBlob) {
+      var s = utf8.decode(obj.blobData);
+      print(s);
+    }
+  }
+
+  if (cmd.name == 'hash-object') {
+    if (cmd.rest.isEmpty) {
+      print('Must provide file path');
+      return;
+    }
+    var filePath = cmd.rest[0];
+    var rawData = File(filePath).readAsBytesSync();
 
     var repo = GitRepository(Directory.current.path);
-    var obj = await repo.readObject(sha1);
-    print(obj);
+    var fmt = cmd['type'] as String;
+    var obj = repo.createObject(fmt, rawData);
+    var sha1Hash = await repo.writeObject(obj, write: cmd['write']);
+    print(sha1Hash);
   }
 }

5fa726548c07a2e5e70577222fb953ceba70ecec
Vishesh Handa <me@vhanda.in>
2020-04-04 01:29:21 +0200

    git remote: Implement a kind of verbose mode

--- lib/commands/remote.dart
+++ lib/commands/remote.dart
@@ -10,13 +10,23 @@ class RemoteCommand extends Command {
   @override
   final description = 'Manage set of tracked repositories';
 
+  RemoteCommand() {
+    argParser.addFlag('verbose', abbr: 'v', defaultsTo: false);
+  }
+
   @override
   Future run() async {
     var gitRootDir = GitRepository.findRootDir(Directory.current.path);
     var repo = await GitRepository.load(gitRootDir);
 
+    var verbose = argResults['verbose'] as bool;
+
     for (var remote in repo.remotes()) {
-      print(remote.name);
+      if (!verbose) {
+        print(remote.name);
+      } else {
+        print('${remote.name}  ${remote.url}');
+      }
     }
   }
 }

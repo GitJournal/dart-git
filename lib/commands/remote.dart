64ad443761796b7078c63e9991414232c64ad1df
Vishesh Handa <me@vhanda.in>
2020-10-08 23:45:24 +0200

    Add 'git remote add' command
    This serves as useful documentation on how to use this library. Also,
    it's very useful for testing, though I should probably add some proper
    automated tests.

--- lib/commands/remote.dart
+++ lib/commands/remote.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:args/command_runner.dart';
 
 import 'package:dart_git/git.dart';
@@ -11,8 +12,11 @@ class RemoteCommand extends Command {
   @override
   final description = 'Manage set of tracked repositories';
 
+  final ArgParser addArgParser = ArgParser();
+
   RemoteCommand() {
     argParser.addFlag('verbose', abbr: 'v', defaultsTo: false);
+    argParser.addCommand('add', addArgParser);
   }
 
   @override
@@ -21,6 +25,21 @@ class RemoteCommand extends Command {
     var repo = await GitRepository.load(gitRootDir);
 
     var verbose = argResults['verbose'] as bool;
+
+    if (argResults.command != null) {
+      var result = argResults.command;
+      if (result.name == 'add') {
+        if (result.arguments.length != 2) {
+          print('usage: git remote add <name> <url>');
+          return;
+        }
+        var name = result.arguments[0];
+        var url = result.arguments[1];
+
+        await repo.addRemote(name, url);
+        return;
+      }
+    }
 
     for (var remote in repo.remotes()) {
       if (!verbose) {

4eeaf34fdd9eaa407fa07d11d7a4e5cc3cec0a2a
Vishesh Handa <me@vhanda.in>
2020-10-12 12:36:03 +0200

    Move common testing code to a library
    This way it'll be easier to write more tests.

--- test/commands/remote_test.dart
+++ test/commands/remote_test.dart
@@ -1,32 +1,18 @@
-import 'dart:async';
-
 import 'package:test/test.dart';
 
-import 'package:dart_git/main.dart' as git;
+import '../lib.dart';
 
 void main() {
   test('remote', () async {
-    var printLog = await run('remote');
+    var printLog = await runDartGitCommand('remote');
     expect(printLog, ['origin']);
   });
 
   test('remote -v', () async {
-    var printLog = await run('remote -v');
+    var printLog = await runDartGitCommand('remote -v');
     expect(printLog, [
       'origin	git@github.com:GitJournal/dart_git.git (fetch)',
       'origin	git@github.com:GitJournal/dart_git.git (push)',
     ]);
   });
-}
-
-Future<List<String>> run(String command) async {
-  var printLog = <String>[];
-
-  var spec = ZoneSpecification(print: (_, __, ___, String msg) {
-    printLog.add(msg);
-  });
-  await Zone.current.fork(specification: spec).run(() {
-    return git.main(command.split(' '));
-  });
-  return printLog;
 }
--- test/commit_test.dart
+++ test/commit_test.dart
@@ -1,12 +1,10 @@
 import 'dart:io';
 
-import 'package:path/path.dart' as p;
-import 'package:process_run/process_run.dart';
-import 'package:process_run/shell.dart' as shell;
 import 'package:test/test.dart';
 
 import 'package:dart_git/git.dart';
 import 'package:dart_git/plumbing/objects/commit.dart';
+import 'lib.dart';
 
 void main() {
   test('Commit all Initial Repo Test - Single File', () async {
@@ -49,71 +47,4 @@ void main() {
     // Do a comparison
     await testRepoEquals(tmpDir2, tmpDir1);
   });
-}
-
-Future<void> runGitCommand(String dir, String command,
-    {Map<String, String> env = const {}}) async {
-  await shell.run(
-    'git $command',
-    workingDirectory: dir,
-    includeParentEnvironment: false,
-    commandVerbose: true,
-    environment: env,
-  );
-}
-
-Future<void> createFile(String basePath, String path, String contents) async {
-  var fullPath = p.join(basePath, path);
-
-  await Directory(p.basename(fullPath)).create(recursive: true);
-  await File(fullPath).writeAsString(contents);
-}
-
-Future<void> testRepoEquals(String repo1, String repo2) async {
-  // Test if all the objects are the same
-  var listObjScript = r'''#!/bin/bash
-set -e
-shopt -s nullglob extglob
-
-cd "`git rev-parse --git-path objects`"
-
-# packed objects
-for p in pack/pack-*([0-9a-f]).idx ; do
-    git show-index < $p | cut -f 2 -d " "
-done
-
-# loose objects
-for o in [0-9a-f][0-9a-f]/*([0-9a-f]) ; do
-    echo ${o/\/}
-done''';
-
-  var script = p.join(Directory.systemTemp.path, 'list-objects');
-  await File(script).writeAsString(listObjScript);
-
-  var repo1Result = await run('bash', [script], workingDirectory: repo1);
-  var repo2Result = await run('bash', [script], workingDirectory: repo2);
-
-  var repo1Objects =
-      repo1Result.stdout.split('\n').where((String e) => e.isNotEmpty).toSet();
-  var repo2Objects =
-      repo2Result.stdout.split('\n').where((String e) => e.isNotEmpty).toSet();
-
-  expect(repo1Objects, repo2Objects);
-
-  // Test if the index is the same
-
-  // Test if all the references are the same
-  var listRefScript = 'git show-ref --head';
-  script = p.join(Directory.systemTemp.path, 'list-refs');
-  await File(script).writeAsString(listRefScript);
-
-  repo1Result = await run('bash', [script], workingDirectory: repo1);
-  repo2Result = await run('bash', [script], workingDirectory: repo2);
-
-  var repo1Refs =
-      repo1Result.stdout.split('\n').where((String e) => e.isNotEmpty).toSet();
-  var repo2Refs =
-      repo2Result.stdout.split('\n').where((String e) => e.isNotEmpty).toSet();
-
-  expect(repo1Refs, repo2Refs);
 }

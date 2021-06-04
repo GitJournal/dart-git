de4ed9617e52644aa3f044361ee3099b5b2945de
Vishesh Handa <me@vhanda.in>
2021-06-04 14:05:53 +0200

    PktLine: Implement the Encoder
    Wow, this took way way longer than I expected.

--- lib/utils/ascii_helper.dart
+++ lib/utils/ascii_helper.dart
@@ -1,4 +1,6 @@
 class asciiHelper {
   static const space = 32;
   static const newLine = 10;
+  static const num0 = 48;
+  static const a = 97;
 }
--- test/plumbing/pkt_line_test.dart
+++ test/plumbing/pkt_line_test.dart
@@ -1,33 +1,22 @@
 import 'dart:convert';
-import 'dart:typed_data';
 
 import 'package:test/test.dart';
 
-class PktLine {
-  static final FlushPkt = Uint8List.fromList([0, 0, 0, 0]);
-  static final FlushPktString = ascii.decode(FlushPkt);
-  static const MaxPayloadSize = 65516;
-  static const OversizePayloadMax = 65520;
-}
-
-class PktLineEncoder {
-  var buffer = BytesBuilder(copy: false);
-
-  void flush() => buffer.add(PktLine.FlushPkt);
-  Uint8List toBytes() => buffer.toBytes();
-
-  void add(String str) {
-    var bytes = utf8.encode(str);
-    buffer.add(bytes);
-  }
-
-  void addBytes(List<int> bytes) {
-    buffer.add(bytes);
-  }
-}
+import 'package:dart_git/plumbing/pkt_line.dart';
 
 void main() {
   group('Encoder', () {
+    test('byteToASCIIHex', () {
+      expect(byteToASCIIHex(5), 53);
+      expect(byteToASCIIHex(15), 102);
+      expect(byteToASCIIHex(ascii.encode('a').first), 184);
+      expect(byteToASCIIHex(ascii.encode('1').first), 136);
+    });
+
+    test('asciiHex16', () {
+      expect(asciiHex16(10), [48, 48, 48, 97]);
+    });
+
     test('Flush', () {
       var encoder = PktLineEncoder();
       encoder.flush();
@@ -39,40 +28,43 @@ void main() {
     test('Encoding', () {
       var testData = [
         ['hello\n', '000ahello\n'],
-        ['hello\n', '0000' /*flush*/, '000ahello\n0000'],
-        ['hello\n', 'world\n', 'foo', '000ahello\n000bworld!\n0007foo'],
+        ['hello\n', PktLine.FlushPktString, '000ahello\n\0\0\0\0'],
+        ['hello\n', 'world!\n', 'foo', '000ahello\n000bworld!\n0007foo'],
         [
           'hello\n',
-          '0000',
-          'world\n',
+          PktLine.FlushPktString,
+          'world!\n',
           'foo',
-          '0000',
-          '000ahello\n0000000bworld!\n0007foo0000'
+          PktLine.FlushPktString,
+          '000ahello\n\0\0\0\0000bworld!\n0007foo\0\0\0\0'
         ],
         [
           'a' * PktLine.MaxPayloadSize,
           'fff0' + 'a' * PktLine.MaxPayloadSize,
         ],
-        [
-          'b' * PktLine.MaxPayloadSize,
-          'b' * PktLine.MaxPayloadSize,
-          // Expected
-          'fff0' + 'a' * PktLine.MaxPayloadSize,
-          'fff0' + 'b' * PktLine.MaxPayloadSize,
-        ],
+        // [
+        //   'b' * PktLine.MaxPayloadSize,
+        //   'b' * PktLine.MaxPayloadSize,
+        //   // Expected
+        //   'fff0' +
+        //       ('a' * PktLine.MaxPayloadSize) +
+        //       'fff0' +
+        //       ('b' * PktLine.MaxPayloadSize),
+        // ],
       ];
       for (var data in testData) {
-        var input = data.sublist(0, data.length - 2);
+        var input = data.sublist(0, data.length - 1);
         var output = data.last;
 
         var encoder = PktLineEncoder();
         for (var i in input) {
-          encoder.add(i);
+          encoder.addLine(utf8.encode(i));
         }
 
-        expect(encoder.toBytes(), output);
+        var actual = encoder.toBytes();
+        expect(actual, utf8.encode(output));
       }
-    }, skip: true);
+    });
 
     // TODO: Error too long
     var _ = [

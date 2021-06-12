eef0c229c381fe918fa602ca86691d44fb65d02b
Vishesh Handa <me@vhanda.in>
2021-06-12 14:49:55 +0200

    We should track fs exceptions properly

---
+++ lib/utils/fs.dart
@@ -0,0 +1,21 @@
+class FileSystemWithResults {
+  // These exceptions are shit -
+  // I don't think I can actually catch all of them!
+  //
+  // createDir
+  // writeFileBytes
+  // deleteFile
+  // list Dir
+  // delete Dir
+
+  // isDirectory
+  // stat
+  // existsSync
+  //
+  // readAsString
+  // set position and read bytes
+  // read as string
+  // contents
+
+  // Put all FS operations in a try catch block?
+}

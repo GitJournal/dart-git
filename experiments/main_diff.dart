854d7f794f7983c36475e51f90b5d4c33708f8df
Vishesh Handa <me@vhanda.in>
2021-06-12 03:06:50 +0200

    main_diff: Make it diff by lines!

--- experiments/main_diff.dart
+++ experiments/main_diff.dart
@@ -1,39 +1,28 @@
 import 'package:diff_match_patch/diff_match_patch.dart';
 import 'package:diff_match_patch/src/diff.dart';
 
-var a = '''Gujarat
-Uttar Pradesh
-Kolkata
-Bihar
-Jammu and Kashmir
+var a = '''one
+two
+three
 ''';
 
-var b = '''Tamil Nadu
-Gujarat
-Andhra Pradesh
-Bihar
-Uttar pradesh
+var b = '''one
+three
+four
 ''';
 
 void main() {
-  // Characters
-  var dmp = DiffMatchPatch();
-  var d = dmp.diff('Hello World.', 'Goodbye World.');
-  dmp.diffCleanupSemantic(d);
-  // Result: [(-1, "Hello"), (1, "Goodbye"), (0, " World.")]
-  print(d);
-
-  // Lines
-  var dmp2 = DiffMatchPatch();
   var res = linesToChars(a, b);
-  /*
-  var lineText1 = res['chars1'];
-  var lineText2 = res['chars2'];
-  var lineArray = res['lineArray'];
-  */
+  var chars1 = res['chars1'] as String;
+  var chars2 = res['chars2'] as String;
+  var lineArray = res['lineArray'] as List<String>;
+  print(chars1.codeUnits);
+  print(chars2.codeUnits);
+  print(lineArray);
 
-  print(dmp2);
-  print('res $res');
+  var dmp = DiffMatchPatch();
+  var diffObjects = dmp.diff(chars1, chars2);
+  charsToLines(diffObjects, lineArray);
+
+  print(diffObjects);
 }
-
-// FIXME: How to convert this to lines?

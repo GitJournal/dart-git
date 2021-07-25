8b42b8a7bc380957724860deb3b835f06ca83188
Vishesh Handa <me@vhanda.in>
2021-07-25 11:23:13 +0200

    Result: Try to always preserve the stackTrace
    In the case when we are throwing an exception, I would prefer throwing
    of a type called ResultException which contains the inner stacktrace,
    otherwise we just get one from the catchAll.

--- lib/utils/result.dart
+++ lib/utils/result.dart
@@ -6,7 +6,9 @@ class Result<DataType> {
   StackTrace? stackTrace;
 
   Result(DataType data) : data = data;
-  Result.fail(Exception error, [this.stackTrace]) : error = error;
+  Result.fail(Exception error, [StackTrace? stackTrace])
+      : error = error,
+        stackTrace = stackTrace ?? StackTrace.current;
   Result._(this.data, this.error, this.stackTrace);
 
   DataType getOrThrow() {
@@ -15,6 +17,9 @@ class Result<DataType> {
     if (data != null) {
       return data!;
     } else {
+      if (stackTrace != null) {
+        throw ResultException(error!, stackTrace!);
+      }
       throw error!;
     }
   }
@@ -29,9 +34,21 @@ class Result<DataType> {
   bool get isSuccess => error == null;
 }
 
+class ResultException implements Exception {
+  final Exception exception;
+  final StackTrace stackTrace;
+
+  ResultException(this.exception, this.stackTrace);
+
+  @override
+  String toString() => exception.toString();
+}
+
 Future<Result<T>> catchAll<T>(Future<Result<T>> Function() catchFn) async {
   try {
     return await catchFn();
+  } on ResultException catch (e) {
+    return Result.fail(e, e.stackTrace);
   } on Exception catch (e, stackTrace) {
     return Result.fail(e, stackTrace);
   }
@@ -57,7 +74,11 @@ extension ResultFuture<T> on Future<Result<T>> {
 
 Result<A> fail<A, B>(Result<B> result) {
   assert(result.error != null);
-  return Result<A>.fail(result.error!, result.stackTrace);
+  if (result.stackTrace != null) {
+    return Result<A>.fail(result.error!, result.stackTrace);
+  } else {
+    return Result<A>.fail(result.error!, StackTrace.current);
+  }
 }
 
 /// Rust style try? operator.

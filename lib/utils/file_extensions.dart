f7a468fe47bda4e5f231c2e38da530ffdc3944c3
Vishesh Handa <me@vhanda.in>
2021-12-09 15:34:06 +0100

    Use posix library instead of our fork of 'os'
    It contains the chmod function!

--- lib/utils/file_extensions.dart
+++ lib/utils/file_extensions.dart
@@ -1,16 +1,16 @@
 import 'dart:io' as io;
 
 import 'package:file/file.dart';
-import 'package:os/file_system.dart' as os_fs;
+import 'package:posix/posix.dart' as posix;
 
 extension FSExtensions on File {
   Future<void> chmod(int mode) async {
-    os_fs.chmodSync(io.File(path), mode);
+    posix.chmod(path, mode.toRadixString(8));
   }
 }
 
 extension FSExtensions2 on io.File {
   Future<void> chmod(int mode) async {
-    os_fs.chmodSync(io.File(path), mode);
+    posix.chmod(path, mode.toRadixString(8));
   }
 }

eeccddd13d13e4d87c07ab147a0ed95602e92f4e
Vishesh Handa <me@vhanda.in>
2021-12-10 11:31:36 +0100

    Revert "Use posix library instead of our fork of 'os'"
    This reverts commit f7a468fe47bda4e5f231c2e38da530ffdc3944c3.

--- lib/utils/file_extensions.dart
+++ lib/utils/file_extensions.dart
@@ -1,16 +1,16 @@
 import 'dart:io' as io;
 
 import 'package:file/file.dart';
-import 'package:posix/posix.dart' as posix;
+import 'package:os/file_system.dart' as os_fs;
 
 extension FSExtensions on File {
   Future<void> chmod(int mode) async {
-    posix.chmod(path, mode.toRadixString(8));
+    os_fs.chmodSync(io.File(path), mode);
   }
 }
 
 extension FSExtensions2 on io.File {
   Future<void> chmod(int mode) async {
-    posix.chmod(path, mode.toRadixString(8));
+    os_fs.chmodSync(io.File(path), mode);
   }
 }
